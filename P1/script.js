10b0e10162891b6bf30deba8555ead4977f5ab50
Anjali12219383 <ac6129994@gmail.com>
2025-06-17 10:37:57 +0530

    updatet

--- P1/script.js
+++ P1/script.js
@@ -1,6 +1,10 @@
 const loginForm=()=>{
     const str=`
     <h3>Login Form</h3>
+    <label for="email">Email:</label>
+    <input type="email" placeholder="email" id="email" required>
+    <label for="password">Password:</label>
+    <input type="password" placeholder="password" id="password" required>
     <p><button onclick="showHome()">submit</button></p>
     <p><button onclick="registerForm()">Create Account</button></p>
     `
@@ -10,6 +14,9 @@ const loginForm=()=>{
 const registerForm=()=>{
      const str=`
     <h3>registration Form</h3>
+    <input type="text" placeholder="name" id="name" required>
+    <input type="email" placeholder="email" id="email" required>
+    <input type="password" placeholder="password" id="password" required>
     <p><button onclick="loginForm()">submit</button></p>
     <p><button onclick="loginForm()">already member?login here</button></p>
     `
